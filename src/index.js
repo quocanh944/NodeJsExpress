57b70c971fbbf4d96c6472615a7b6fffe9328845
Luc Minh Hieu <52100889@student.tdtu.edu.vn>
2023-10-17 20:01:47 +0700

    import models

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ setupDB();
 import user from './models/user.js';
 import customer from './models/customer.js';
 import product from './models/product.js';
-import transaction from './models/transaction.js';
+import order from './models/order.js';
+import productCount from './models/productCount.js';
 
 // Setup View Engine
 configureViewEngine(app);
