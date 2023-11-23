b37384a89ae9855d325e42367e6e6731a30b5caf
Lục Minh Hiếu <52100889@student.tdtu.edu.vn>
2023-11-23 17:00:26 +0000

    Update productCount.js

--- src/models/cartItem.js
+++ src/models/cartItem.js
@@ -1,10 +1,10 @@
-// productCount.js
+// cartItem.js
 import mongoose from 'mongoose';
 
-const productCountSchema = new mongoose.Schema({
+const cartItemSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
     quantity: Number
 });
 
-export default mongoose.model('ProductCount', productCountSchema);
+export default mongoose.model('CartItem', cartItemSchema);
